bd555c0770041b4bd7d55b39a5e52ee1bb19abb8
Aleksandr Vinogradov <iiojib.vi@gmail.com>
2020-06-03 13:12:47 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "region" {
   description = "Region"
+  default = "us-east-1"
 }
 
 variable "instance_type" {
   description = "Instance Type"
+  default = "t2.nano"
 }
 
 variable "instance_count" {
@@ -12,12 +14,14 @@ variable "instance_count" {
 }
 
 variable "subnet" {
-description = "Subnet ID"
+  description = "Subnet ID"
+  default = "subnet-41b49e24"
 }
 
 variable "sg" {
   description = "AWS Secruity Group"
   type        = list(string)
+  default     = ["sg-307ea059", "sg-24319f5f"]
 }
 
 variable "key" {
@@ -25,9 +29,11 @@ variable "key" {
 }
 
 variable "vpc_id" {
-description = "VPC"
+  description = "VPC"
+  default = "vpc-596aa03e"
 }
 
 variable "ami" {
   description = "AMI"
+  default = "ami-0018d24682ffadf55"
 }
